3b18a5e99425ac4b588b359a9365b635a63f6503
Bea Gomes <112278018+zirtaebea@users.noreply.github.com>
2023-07-25 21:37:42 -0300

    Update 06_oficina_users.sql

--- 06_oficina_users.sql
+++ 06_oficina_users.sql
@@ -1,4 +1,4 @@
-use Projeto_final;
+use oficina_db;
 
 -- 1) criando usuarios
 -- financeiro
@@ -12,35 +12,35 @@ CREATE USER 'administrativo'@'localhost'IDENTIFIED BY 'Administra@1234$';
 
 -- 2) permissões do user financeiro
 -- apenas modificar a tabela pedido e financeiro, o resto só leitura
-GRANT SELECT ON Projeto_final.historico_alunos TO 'financeiro'@'localhost';
-GRANT SELECT ON Projeto_final.alunos TO 'financeiro'@'localhost';
-GRANT SELECT ON Projeto_final.turmas TO 'financeiro'@'localhost';
+GRANT SELECT ON oficina_db.historico_alunos TO 'financeiro'@'localhost';
+GRANT SELECT ON oficina_db.alunos TO 'financeiro'@'localhost';
+GRANT SELECT ON oficina_db.turmas TO 'financeiro'@'localhost';
 -- modo leitura
-GRANT SELECT, UPDATE, INSERT ON Projeto_final.pedidos TO 'financeiro'@'localhost';
-GRANT SELECT, UPDATE, INSERT ON Projeto_final.financeiro TO 'financeiro'@'localhost';
+GRANT SELECT, UPDATE, INSERT ON oficina_db.pedidos TO 'financeiro'@'localhost';
+GRANT SELECT, UPDATE, INSERT ON oficina_db.financeiro TO 'financeiro'@'localhost';
 
 -- mostrando permissões
 SHOW GRANTS FOR 'financeiro'@'localhost';
 
 -- 3) permissões user rh
 -- apenas leitura
-GRANT SELECT ON Projeto_final.historico_alunos TO 'rh'@'localhost';
-GRANT SELECT ON Projeto_final.alunos TO 'rh'@'localhost';
-GRANT SELECT ON Projeto_final.turma TO 'rh'@'localhost';
-GRANT SELECT ON Projeto_final.pedidos TO 'rh'@'localhost';
-GRANT SELECT ON Projeto_final.financeiro TO 'rh'@'localhost';
+GRANT SELECT ON oficina_db.historico_alunos TO 'rh'@'localhost';
+GRANT SELECT ON oficina_db.alunos TO 'rh'@'localhost';
+GRANT SELECT ON oficina_db.turma TO 'rh'@'localhost';
+GRANT SELECT ON oficina_db.pedidos TO 'rh'@'localhost';
+GRANT SELECT ON oficina_db.financeiro TO 'rh'@'localhost';
 
 -- mostrando permissões
 SHOW GRANTS FOR 'rh'@'localhost';
 
 -- 4) permissões do user administrativo
 -- apenas modificar historico_alunos, alunos e turma
-GRANT SELECT, UPDATE, INSERT ON Projeto_final.historico_alunos TO 'administrativo'@'localhost';
-GRANT SELECT, UPDATE, INSERT ON Projeto_final.alunos TO 'administrativo'@'localhost';
-GRANT SELECT, UPDATE, INSERT ON Projeto_final.turmas TO 'administrativo'@'localhost';
+GRANT SELECT, UPDATE, INSERT ON oficina_db.historico_alunos TO 'administrativo'@'localhost';
+GRANT SELECT, UPDATE, INSERT ON oficina_db.alunos TO 'administrativo'@'localhost';
+GRANT SELECT, UPDATE, INSERT ON oficina_db.turmas TO 'administrativo'@'localhost';
 -- modo leitura em pedidos e financeiro
-GRANT SELECT ON Projeto_final.pedidos TO 'administrativo'@'localhost';
-GRANT SELECT ON Projeto_final.financeiro TO 'administrativo'@'localhost';
+GRANT SELECT ON oficina_db.pedidos TO 'administrativo'@'localhost';
+GRANT SELECT ON oficina_db.financeiro TO 'administrativo'@'localhost';
 
 -- mostrando permissões
 SHOW GRANTS FOR 'administrativo'@'localhost';
